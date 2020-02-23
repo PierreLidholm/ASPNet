8520b22bb1592afe7c1a226334062ffd624328c5
Pierre Lidholm <Pirrelidholm@gmail.com>
2020-02-23 14:59:20 +0100

    Add project files.

---
+++ PierreLidholm/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PierreLidholm.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Item1()
+        {
+            return View();
+        }
+
+        public IActionResult Item2()
+        {
+            return View();
+        }
+
+        public IActionResult Item3()
+        {
+            return View();
+        }
+    }
+}
---
+++ PierreLidholm/Models/AccountManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PierreLidholm.Models
+{
+    public class AccountManager : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public int PostalCode { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+
+    }
+}
